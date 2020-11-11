619fb52ae2b4429f8065c9f70e2a90bd6de65900
Icecold-1 <49763399+Icecold-1@users.noreply.github.com>
2020-11-11 11:51:25 +0100

    Add files via upload

--- postgres/db-create.sql
+++ postgres/db-create.sql
@@ -1,16 +1,24 @@
-create table nas (na raw(512));
+create table nas (
+	na bytea not null, 
+	primary key(na)
+);
 
 create table handles (
-	handle raw(512),
-	idx number(10),
-	type raw(128),
-	data blob,
-	ttl_type number(5),
-	ttl number(10),
-	timestamp number(10),
-	refs varchar2(512),
-	admin_read varchar2(5),
-	admin_write varchar2(5),
-	pub_read varchar2(5),
-	pub_write varchar2(5)
+	handle bytea not null, 
+	idx int4 notnull, 
+	type bytea, 
+	data bytea, 
+	ttl_type int2, 
+	ttl int4, 
+	timestamp int4, 
+	refs text, 
+	admin_read bool,
+	admin_write bool, 
+	pub_read bool,
+	pub_write bool, 
+	primary key(handle, idx)
 );
+
+create index dataindex on handles ( data );
+
+create index handleindex on handles ( handle );
