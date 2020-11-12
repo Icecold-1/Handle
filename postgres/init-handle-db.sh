e94abfcd7c0d8d53924cd167fd4184f0b331fe84
Icecold-1 <49763399+Icecold-1@users.noreply.github.com>
2020-11-12 13:17:10 +0100

    Add files via upload

--- postgres/init-handle-db.sh
+++ postgres/init-handle-db.sh
@@ -4,11 +4,11 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER handle_admin WITH PASSWORD '4neCap2tpVctsVpv';
-    CREATE DATABASE handle_db;
-    GRANT ALL PRIVILEGES ON DATABASE handle_db TO handle_admin;
+    CREATE DATABASE "handle_db";
+    GRANT ALL PRIVILEGES ON DATABASE "handle_db" TO handle_admin;
 EOSQL
 
-psql -v ON_ERROR_STOP=1 --username "handle_admin" --database "handle" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "handle_admin" --database "handle_db" <<-EOSQL
 	CREATE TABLE nas (na bytea not null, primary key(na));
 	CREATE TABLE handles (handle bytea not null, idx int4 not null, type bytea, data bytea, ttl_type int2, ttl int4, timestamp int4, refs text, admin_read bool, admin_write bool, pub_read bool, pub_write bool, primary key(handle, idx));
 	CREATE INDEX dataindex on handles ( data );
