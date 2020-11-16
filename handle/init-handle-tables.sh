93c3070e7da0c3090eb06a16ed06653d6589266f
Icecold-1 <49763399+Icecold-1@users.noreply.github.com>
2020-11-16 15:01:32 +0100

    Add files via upload

---
+++ handle/init-handle-tables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_DBUSER" --password "$POSTGRES_DBPASS" -d "$POSTGRES_DBNAME" <<-EOSQL
+	CREATE TABLE nas (na bytea not null, primary key(na));
+	CREATE TABLE handles (handle bytea not null, idx int4 not null, type bytea, data bytea, ttl_type int2, ttl int4, timestamp int4, refs text, admin_read bool, admin_write bool, pub_read bool, pub_write bool, primary key(handle, idx));
+	CREATE INDEX dataindex on handles ( data );
+	CREATE INDEX handleindex on handles ( handle );
+	GRANT all on "nas","handles" to $POSTGRES_DBUSER;
+	GRANT select on "nas","handles" to public;
+EOSQL
