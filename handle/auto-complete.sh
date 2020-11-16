46819b09e58cdec6b2cbaf26c6b2c81dd1af7f9e
Icecold-1 <49763399+Icecold-1@users.noreply.github.com>
2020-11-16 09:16:43 +0100

    Add files via upload

--- handle/auto-complete.sh
+++ handle/auto-complete.sh
@@ -4,6 +4,8 @@ echo "generating responses..."
 
 cd /handle-server/handle-9.3.0/bin/
 
-/genrsp.sh response.txt
+touch response.txt
 
-cat response.txt | ./hdl-setup-server /hdl_svr
+/genrsp.sh /handle-server/handle-9.3.0/bin/response.txt
+
+cat /handle-server/handle-9.3.0/bin/response.txt | ./hdl-setup-server /hdl_svr
